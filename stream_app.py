ce99e9180798f3c6da124072114b3585f8836c08
vm221247 <venkatesh.8.maddukuri@gsk.com>
2023-07-08 15:24:32 +0530

    change

--- stream_app.py
+++ stream_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Parents new healthy Dinner')
+streamlit.title('My Parents new healthy Dinner ??')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
