fc95f97d5a30d5adaba44083d03a09b047ddb279
VenkyJishu <57663599+VenkyJishu@users.noreply.github.com>
2023-07-08 14:38:18 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy Dinner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu !!!')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
