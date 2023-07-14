6a8375f9f914370b460769ffd3ff7ed0e00305cc
vm221247 <venkatesh.8.maddukuri@gsk.com>
2023-07-14 22:42:14 +0530

    new

--- stream_app.py
+++ stream_app.py
@@ -4,7 +4,7 @@ my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select current_user(),current_account(),current_region()")
 my_data_row=my_cur.fetchone()
-streamlit.text("Hello from snowflake")
+streamlit.header("Hello from snowflake")
 streamlit.text(my_data_row)
 
 
