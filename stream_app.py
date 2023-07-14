27f8f683cd41a5979288d939a630b676ddd75241
vm221247 <venkatesh.8.maddukuri@gsk.com>
2023-07-14 22:38:50 +0530

    new

--- stream_app.py
+++ stream_app.py
@@ -1,5 +1,12 @@
 import streamlit
 import snowflake.connector
+my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select current_user(),current_account(),current_region()")
+my_data_row=my_cur.fetchone()
+streamlit.text("Hello from snowflake")
+streamlit.text(my_data_row)
+
 
 streamlit.title('My Parents new healthy Dinner ??')
 streamlit.header('Breakfast Menu')
