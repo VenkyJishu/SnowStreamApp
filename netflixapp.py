17e4b096005bee8c400209ff5adea616916b4128
VenkyJishu <57663599+VenkyJishu@users.noreply.github.com>
2024-05-28 23:56:45 +0530

    Update netflixapp.py

--- netflixapp.py
+++ netflixapp.py
@@ -9,6 +9,8 @@ def app():
         if st.button('Login'):
             
             st.write("Login is Successful")
+        if st.button("Forgot Password"):
+            st.text_input("Please enter your mail to send temporary password")
     else:
         mail =st.text_input("Email Address")
         username = st.text_input("Enter Username")
